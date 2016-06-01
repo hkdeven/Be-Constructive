4f538cc542ad8b65e7860c6766c23dab60f4f05e
Deven Blackburn <deven@codefll.org>
2016-05-31 22:48:12 -0400

    Upload friend images via Paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,10 +80,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('AWS_REGION'),
+    bucket: ENV.fetch('be-constructive'),
+    access_key_id: ENV.fetch('AKIAIMCR3PLH5NVWKICQ'),
+    secret_access_key: ENV.fetch('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'),
+    s3_region: ENV.fetch('us-east-1'),
   }
 }
 end
